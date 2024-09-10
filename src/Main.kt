2a225426c7794691d527d32a0fb75432109068ee
Admin <reteamboss@gmail.com>
2024-09-10 19:42:24 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello World!")
+    var argument: IntArray = intArrayOf(1,2,3,4,5)
+    println(checkArg(argument))
+
+}
+
+fun<T> checkArg(argument: T): String {
+    var result = ""
+    when(argument){
+        is String -> result = (argument.length + 1).toString()
+        is Int -> result = (argument.toInt() + 1).toString()
+        is IntArray -> result = argument.sum().toString()
+        else -> result ="Не знаю что это"
+
+    }
+    return result
 }
